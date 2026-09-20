73e9f5f5429d0f4dbdcef81a1d937f783d13db4d
Tulipskun <103128996+Tulipskun@users.noreply.github.com>
2026-09-20 15:13:56 +0700

    Remove hardcoded Cloudflare hostname

--- app/src/main/java/com/tulipskun/droidmcp/CloudflareTunnelConfig.kt
+++ app/src/main/java/com/tulipskun/droidmcp/CloudflareTunnelConfig.kt
@@ -3,5 +3,4 @@ package com.tulipskun.droidmcp
 object CloudflareTunnelConfig {
     const val LOCAL_PORT = 8787
     const val LOCAL_ORIGIN = "http://127.0.0.1:8787"
-    const val DEFAULT_HOSTNAME = "droid.box.dotenvlab.dev"
 }
